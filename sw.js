42cc5889600bc768fe2bf41571d89778ba024e2d
prkxf <prkxfzmfx@gmail.com>
2026-07-20 15:21:51 +0900

    バックアップHTTP422修正: クラウドAPIのGETをcache:no-storeに、422/409時はsha取り直しで1回再試行（応用情報アプリで発生した同型バグの予防移植）、SWキャッシュv5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ネットワーク優先・失敗時キャッシュ（オフラインでも起動できるように）
-const CACHE = 'kakeibo-v4';
+const CACHE = 'kakeibo-v5';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
